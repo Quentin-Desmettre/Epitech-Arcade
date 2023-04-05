f457731869a7a62f5975a3e4b59e34be8db76451
PaulinLec <paulin.leclercq@epitech.eu>
2023-04-05 14:16:49 +0200

    fix(SFML): binding correctly keys in SFML and removing wildcard in centipede Makefile

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -10,11 +10,11 @@
 
 int main(int ac, char **av)
 {
-    try {
+   try {
         Arcade::Core::Core core(ac, av);
         return core.run(av[1]);
-    } catch (std::exception &e) {
-        std::cerr << e.what() << std::endl;
-        return 84;
-    }
+   } catch (std::exception &e) {
+       std::cerr << e.what() << std::endl;
+       return 84;
+   }
 }
